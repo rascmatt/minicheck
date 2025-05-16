9e8ec9126a77d2a89bd538fff2fb9792870b1f44
Matthias Raschhofer <e11908082@student.tuwien.ac.at>
2025-05-16 12:47:43 +0200

    Implement and improve parsing of program arguments

---
+++ app/Lib/Parser/Options.hs
@@ -0,0 +1,64 @@
+module Lib.Parser.Options (Options, oArgs, oTsFile, oHelp, oIllegal, parse) where
+
+import Lib.Parser.Base
+
+data Options = Options {
+    oArgs    :: [String],
+    oTsFile  :: String,
+    oHelp    :: Bool,
+    oIllegal :: [String] -- Unparsable arguments
+} deriving (Show)
+
+mOptional :: Parse String -> Parse String
+mOptional p = pure "" `mplus` p
+
+mFlag :: String -> Parse Bool
+mFlag key = do
+    _ <- skipWs (string "--")
+    _ <- string key
+    f <- mOptional (do {
+        _ <- skipWs (string "=");
+        (neList . sat) (not . isWs)
+    })
+    case f of
+        "true"  -> return True
+        ""      -> return True
+        "false" -> return False
+        _       -> mzero
+
+mOption :: String -> Parse String
+mOption key = do
+    _ <- skipWs (string "--")
+    _ <- string key
+    _ <- skipWs (string "=")
+    (neList . sat) (not . isWs)
+
+mArg :: Parse String
+mArg = do
+    c <- skipWs (sat (/= '-'))
+    s <- (list . sat) (not . isWs)
+    return (c:s)
+
+option :: String -> String -> Maybe String
+option key = topLevel (mOption key)
+
+arg :: String -> Maybe String
+arg = topLevel mArg
+
+flag :: String -> String -> Maybe Bool
+flag key = topLevel (mFlag key)
+
+parse :: [String] -> Options
+parse args = Options ar tsFile hFlag illegal
+    where
+        parsed  = [ (a, arg a, option "ts" a, flag "help" a) | a <- args ]
+        -- Collect everything we can't parse at all
+        illegal = [ a | (a, Nothing, Nothing, Nothing) <- parsed ]
+        -- If we can't parse it to a flag or an option, its a normal argument
+        ar      = [ x | (_, Just x, Nothing, Nothing) <- parsed ]
+        ts      = [ tt | (_, _, Just tt, _) <- parsed ]
+        -- Use the last occurrence of the '--ts' flag
+        tsFile  = if null ts then [] else last ts
+        -- Use the last occurrence of the '--help' flag
+        help    = [ x | (_, Nothing, Nothing, Just x) <- parsed ]
+        hFlag   = not (null help) && last help
--- app/Main.hs
+++ app/Main.hs
@@ -4,20 +4,42 @@ import System.Environment (getArgs)
 import Data.Maybe (isNothing, fromJust)
 import System.Exit (exitFailure)
 
+import qualified Lib.Parser.Options as Options
 import Lib.Parser.TS (parse, validate)
+
+-- Validate options and exit in case of invalid arguments
+validateOpts :: Options.Options -> IO ()
+validateOpts options = do
+
+    -- Check for illegal arguments
+    if (not . null) (Options.oIllegal options) then do
+        putStr "Invalid arguments: "
+        print (Options.oIllegal options)
+        putStrLn "Usage: minicheck --ts=filename [--help]"
+        exitFailure
+    else do { return () }
+
+    -- Print the usage info if the '--help' flag is used
+    if Options.oHelp options then do
+        putStrLn "Usage: minicheck --ts=filename [--help]"
+        exitFailure
+    else do { return () }
+
+    -- Assert that we have an input TS file
+    if null (Options.oTsFile options) then do
+        putStrLn "Missing input specification. "
+        putStrLn "Usage: minicheck --ts=filename [--help]"
+        exitFailure
+    else do { return () }
 
 main :: IO ()
 main = do
 
-    -- TODO: Improve argument parsing
-    args    <- getArgs
-    fileName    <- case args of
-        [n]  -> return n
-        _    -> do
-            putStrLn "Usage: minicheck 'filename'"
-            exitFailure
-
-    content <- readFile fileName
+    args <- getArgs
+    let options = Options.parse args
+    validateOpts options
+    
+    content <- readFile (Options.oTsFile options)
     let ts = parse content
     let v  = validate ts -- TODO: Maybe print a better validation message
     if isNothing ts then do
@@ -27,8 +49,8 @@ main = do
         if isNothing v then do
             putStrLn "Invalid specification"
             exitFailure
-        else do 
+        else do
             print (fromJust v)
 
     -- The transition system is ready at this point
-    
+
