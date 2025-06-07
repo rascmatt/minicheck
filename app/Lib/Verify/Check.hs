a67b89bc8521b26a450c814af304fca4e3313f5a
Matthias Raschhofer <e11908082@student.tuwien.ac.at>
2025-06-07 13:37:25 +0200

    Implement model checking algorithm
    - Implements algorithm 1 (with 2 and 3)

--- app/Lib/Verify/Check.hs
+++ app/Lib/Verify/Check.hs
@@ -1,7 +1,9 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module Lib.Verify.Check (verify) where
 
 import Lib.Model.CTL (CTL(..), LogicalOperator(..), PathFormula(..))
-import Lib.Model.TS (TS)
+import Lib.Model.TS (TS, State, states, labels, state, prop, lProp, trans, from, to, initial)
+import Data.List (nub, (\\))
 
 data ENF -- Existential Normal Form
   = ETruth
@@ -29,9 +31,78 @@ toENF (Exists (Eventually x)) = EUntil ETruth (toENF x)
 toENF (Exists (Globally x)) = EGlobally (toENF x)
 toENF (ForAll (Next x)) = ENegation (ENext (ENegation $ toENF x))
 toENF (ForAll (Until x y)) = ENegation (EConjunction (EUntil (ENegation $ toENF y) (EConjunction (ENegation $ toENF x) (ENegation $ toENF y))) (ENegation (EGlobally (ENegation $ toENF y))))
-toENF (ForAll (Eventually x)) = toENF $ ForAll (Until Truth x)
+toENF (ForAll (Eventually x)) = ENegation (EGlobally (ENegation $ toENF x))
 toENF (ForAll (Globally x)) = toENF $ Negation (Exists (Eventually (Negation x)))
 
--- Does the CTL formula hold in the given TS?
+-- TODO: Assert that all propositions in CTL are also in TS
+
 verify :: TS -> CTL -> Bool
-verify _ _ = False
+verify ts ctl = ia `subset` satSet
+  where
+    satSet = satFun ts (toENF ctl)
+    ia     = initial ts
+
+satFun :: TS -> ENF -> [State]
+
+-- Sat(true) := S
+satFun ts ETruth = states ts
+
+-- Sat(a) := { s ∈ S | a ∈ L(s) }, for any a ∈ AP
+satFun ts (EAtomicProposition p) = statesWithLabel ts p
+  where statesWithLabel t x = (nub . map state . filter (\l -> x == prop (lProp l))) (labels t)
+
+-- Sat(ɸ && Ψ) := Sat(ɸ) ∩ Sat(Ψ)
+satFun ts (EConjunction a b) = satFun ts a `intersect` satFun ts b
+
+-- Sat(⌐ɸ) := S \ Sat(ɸ)
+satFun ts (ENegation p) = filter (`notElem` satFun ts p) (states ts)
+
+-- Sat(∃X ɸ) := { s ∈ S | (Post(s) ∩ Sat(ɸ)) != ∅ }
+satFun ts (ENext p) = (nub . filter hasNext) (states ts)
+  where
+    satP      = satFun ts p
+    hasNext s = (not . null) (post ts s `intersect` satP)
+
+-- Sat(∃(ɸ 𝒰 Ψ))
+satFun ts (EUntil p q) = satFunUntil ts satP t
+  where
+    t = satFun ts q
+    satP = satFun ts p
+
+-- Sat(∃G ɸ)
+satFun ts (EGlobally p) = satFunGlobally ts t
+  where t = satFun ts p
+
+satFunGlobally :: TS -> [State] -> [State]
+satFunGlobally ts t
+  | null del  = t
+  | otherwise = satFunGlobally ts newT
+    where
+      del = [s | s <- t, null (post ts s `intersect` t)]
+      newT = t `diff` del
+
+satFunUntil :: TS -> [State] -> [State] -> [State]
+satFunUntil ts satP t
+  | null add  = t
+  | otherwise = satFunUntil ts satP newT
+    where
+      add = [ s | s <- satP `diff` t, (not . null) (post ts s `intersect` t)]
+      newT = t `union` add
+
+-- Utility functions
+
+post :: TS -> State -> [State]
+post ts s = (map to . filter (\t -> from t == s)) (trans ts)
+
+intersect :: (Eq a) => [a] -> [a] -> [a]
+intersect xs ys = filter (`elem` ys) xs
+
+union :: (Eq a) => [a] -> [a] -> [a]
+union xs ys = nub (xs ++ ys)
+
+diff :: (Eq a) => [a] -> [a] -> [a]
+diff xs ys = xs \\ ys
+
+subset :: (Eq a) => [a] -> [a] -> Bool
+subset xs ys = all (`elem` ys) xs
+
